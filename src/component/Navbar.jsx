69fa8dc980bc6f7af0af469eb065d581bb68e740
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-09-17 17:28:01 +0200

    start

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,10 +3,15 @@ import { NavLink } from "react-router";
 export default function Navbar() {
     return(
         <nav>
-            <NavLink to="/" end>Home</NavLink>
-            <NavLink to="/project">Project</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <div className="navigation 1">
+                <NavLink to="/" end>Home</NavLink>
+                <NavLink to="/project">Project</NavLink>
+            </div>
+
+            <div className="navigation 2">
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
         </nav>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 export default function Home() {
     return (
-        <h1>Home side</h1>
+        <h1>Multimediedesigner</h1>
     );
 }
+
