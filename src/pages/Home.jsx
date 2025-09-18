33528d16be34b24c8450eb8b94863fa2fb7d78b8
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-09-18 13:48:10 +0200

    billedere og forside start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,26 @@
+
 export default function Home() {
     return (
-        <h1>Multimediedesigner</h1>
+        <>
+            <header>
+                <figure className="headerImage">
+                    <img src="./billeder/me-upside-down.png" alt="" />
+                
+                <div className="intro">
+                    <p>Hi, I'm a</p>
+                    <h1>Multimediadesigner</h1>
+                </div>
+                </figure>
+
+                
+            </header>
+
+            <footer>
+                <figure className="footerImage">
+                    <img src='./billeder/me-one.png' alt="" />
+                </figure>
+            </footer>
+        </>
     );
 }
 
