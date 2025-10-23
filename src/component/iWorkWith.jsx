d9e807f8d53a6c9046481f3c0047e47a7993f0cd
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-10-23 22:43:04 +0200

    Refactor SVG files and update styles for improved visuals
    - Updated `html.svg` and `javascript.svg` files to enhance SVG structure and styling.
    - Modified `iWorkWith.jsx` to include a new layout for displaying programming and design tools.
    - Created a new CSS file `iWorkWith.css` to style the new layout and elements.
    - Adjusted existing CSS in `home.css` and `info.css` for consistency and improved design.

--- src/component/iWorkWith.jsx
+++ src/component/iWorkWith.jsx
@@ -2,13 +2,64 @@ import '../css/iWorkWith.css';
 
 export default function IWorkWith() {
     return (
-        <section>
+        <section className='slide-section'>
             <figure className='workWith'>
                 <img src="./billeder/caption-box.svg" alt="" />
                 <h2>I work with</h2>
             </figure>
 
-            
+            <div className='slide-bar'>
+                <div className='programming'>
+                    <figure>
+                        <img className='app' src="./billeder/html.svg" alt="" />
+                        <img className='one' src="./billeder/icon-box.svg" alt="" />
+                    </figure>
+
+                    <figure>
+                        <img className='app' src="./billeder/css.svg" alt="" />
+                        <img className='two' src="./billeder/icon-box.svg" alt="" />
+                    </figure>
+
+                    <figure>
+                        <img className='app' src="./billeder/javascript.svg" alt="" />
+                        <img className='three' src="./billeder/icon-box.svg" alt="" />
+                    </figure>
+                </div>
+
+                <figure className='b-slide'>
+                    <img className='slide' src="./billeder/background-slide.svg" alt="" />
+                    <img className='color' src="./billeder/color-splash.jpg" alt="" />
+
+                    <div className='hand'>
+                        <img src="./billeder/hand.svg" alt="" />
+                    </div>
+                </figure>
+
+                <div className='design'>
+                    <figure>
+                        <img className='crea-app' src="./billeder/illustrator.svg" alt="" />
+                        <img className='crea-one' src="./billeder/icon-box.svg" alt="" />
+                    </figure>
+
+                    <figure>
+                        <img className='crea-app' src="./billeder/photoshop.svg" alt="" />
+                        <img className='crea-two' src="./billeder/icon-box.svg" alt="" />
+                    </figure>
+
+                    <figure>
+                        <img className='crea-app' src="./billeder/figma.svg" alt="" />
+                        <img className='crea-three' src="./billeder/icon-box.svg" alt="" />
+                    </figure>
+                </div>
+            </div>
+
+            <figure className='detail'>
+                <img src="./billeder/slidepanel-detail.svg" alt="" />
+            </figure>
+
+            <figure className='split3'>
+                <img src="./billeder/section-split-3.png" alt="" />
+            </figure>
 
         </section>
     );
