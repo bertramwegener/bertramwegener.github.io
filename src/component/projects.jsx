cdd3a466194ac2adef297d593425db6aa6325ba4
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-10-24 00:12:31 +0200

    Refactor code structure for improved readability and maintainability

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -2,35 +2,51 @@ import '../css/projects.css';
 
 export default function Cases() {
     return (
-        <>
-            <figure>
+        <section className='case-section'>
+            <figure className='lanterns'>
                 <img src="./billeder/lanterns.svg" alt="" />
             </figure>
             
-            <figure>
+            <figure className='cases'>
                 <img src="./billeder/caption-box.svg" alt="" />
                 <h2>Cases</h2>
             </figure>
 
-            <section>
+            <section className='line1'>
                 <figure>
-                    <img src="./billeder/case-border.svg" alt="" />
+                    <div className='border'>
+                        <img src="./billeder/case-border.svg" alt="" />
+                    </div>
+
+                    <a href="https://bertramwegener.github.io/ZeroBuzz-Brew/" target='_blank'><img className='link' src="./billeder/zerobuzz.png" alt="" /></a>
                 </figure>
 
                 <figure>
-                    <img src="./billeder/case-border.svg" alt="" />
+                    <div className='border'>
+                        <img src="./billeder/case-border.svg" alt="" />
+                    </div>
+
+                    <a href="https://bertramwegener.github.io/webunivers-optimised/" target='_blank'><img className='link2' src="./billeder/arkitonisk.png" alt="" /></a>
                 </figure>
             </section>
 
-            <section>
+            <section className='line2'>
                 <figure>
-                    <img src="./billeder/case-border.svg" alt="" />
+                    <div className='border'>
+                        <img src="./billeder/case-border.svg" alt="" />
+                    </div>
+
+                    <a href="https://bertramwegener.github.io/Endometriose/" target='_blank'><img className='link3' src="./billeder/endometriose.png" alt="" /></a>
                 </figure>
 
                 <figure>
-                    <img src="./billeder/case-border.svg" alt="" />
+                    <div className='border'>
+                        <img src="./billeder/case-border.svg" alt="" />
+                    </div>
+
+                    <a href="https://bertramwegener.github.io/Radar/" target='_blank'><img className='link4' src="./billeder/radar.png" alt="" /></a>
                 </figure>
             </section>
-        </>
+        </section>
     );
 }
